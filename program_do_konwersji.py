21814e445819dd7f1266f6e4326f786ab657875e
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-06-14 13:48:35 +0200

    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json

--- program_do_konwersji.py
+++ program_do_konwersji.py
@@ -1,6 +1,18 @@
 import argparse
 import json
+import yaml
+import xml.etree.ElementTree as ET
 
-if input_extension == ".json":
-    with open(input_file, "r") as file:
-        data = json.load(file)
+if input_extension == ".json" and (output_extension == ".xml" or output_extension == ".yaml" or output_extension == ".yml"):
+    root = ET.Element("Data")
+    for key, value in data.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    tree = ET.ElementTree(root)
+    if output_extension == ".xml":
+        tree.write(output_file)
+        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
+    elif output_extension == ".yaml" or output_extension == ".yml":
+        with open(output_file, "w") as file:
+            yaml.dump(data, file)
+        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
