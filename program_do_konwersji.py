4aebc5a701a4d9ee7591cbe4494eee12dd8d1968
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-06-14 13:13:10 +0200

    wczytywanie do obiektu z pliku json

--- program_do_konwersji.py
+++ program_do_konwersji.py
@@ -1,12 +1,6 @@
 import argparse
+import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file", help="sciezka do pliku wejsciowego")
-parser.add_argument("output_file", help="sciezka do pliku wyjsciowego")
-args = parser.parse_args()
-
-input_file = args.input_file
-output_file = args.output_file
-
-input_extension = os.path.splitext(input_file)[1].lower()
-output_extension = os.path.splitext(output_file)[1].lower()
+if input_extension == ".json":
+    with open(input_file, "r") as file:
+        data = json.load(file)
