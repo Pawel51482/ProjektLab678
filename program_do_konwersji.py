ff0840d91d8087634a89d697d2c679713072dd00
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-06-14 13:52:35 +0200

    wczytywanie do obiektu z pliku .xml

--- program_do_konwersji.py
+++ program_do_konwersji.py
@@ -1,18 +1,9 @@
 import argparse
-import json
-import yaml
 import xml.etree.ElementTree as ET
 
-if (input_extension == ".yaml" or input_extension == ".yml") and (output_extension == ".xml" or output_extension == ".json"):
-    root = ET.Element("Data")
-    for key, value in data.items():
-        child = ET.SubElement(root, key)
-        child.text = str(value)
-    tree = ET.ElementTree(root)
-    if output_extension == ".xml":
-        tree.write(output_file)
-        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
-    elif output_extension == ".json":
-        with open(output_file, "w") as file:
-            json.dump(data, file, indent=4)
-        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
+if input_extension == ".xml":
+    try:
+        tree = ET.parse(input_file)
+        root = tree.getroot()
+    except ET.ParseError as e:
+        print(f"Błąd w składni pliku XML: {str(e)}")
