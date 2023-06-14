dc04d79f1986a2269617a1d5bab9548b189400f0
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-06-14 13:53:39 +0200

    zapis danych z obiektu do pliku

--- program_do_konwersji.py
+++ program_do_konwersji.py
@@ -1,9 +1,19 @@
 import argparse
+import json
+import yaml
 import xml.etree.ElementTree as ET
 
-if input_extension == ".xml":
-    try:
-        tree = ET.parse(input_file)
-        root = tree.getroot()
-    except ET.ParseError as e:
-        print(f"Błąd w składni pliku XML: {str(e)}")
+if input_extension == ".xml" and (output_extension == ".json" or output_extension == ".yaml" or output_extension == ".yml"):
+    data = {}
+    for element in root:
+        key = element.tag
+        value = element.text
+        data[key] = value
+    if output_extension == ".json":
+        with open(output_file, "w") as file:
+            json.dump(data, file, indent=4)
+        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
+    elif output_extension == ".yaml" or output_extension == ".yml":
+        with open(output_file, "w") as file:
+            yaml.dump(data, file)
+        print(f"Dane zostaly przekonwertowane do pliku {output_file}")
